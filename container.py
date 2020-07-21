5b77e883c29445256e69590fb5f394b7512cd5f5
hjk <joo6077@gmail.com>
2020-07-21 21:11:51 +0900

    list, loop

---
+++ container.py
@@ -0,0 +1,6 @@
+#데이터 형식을 알려줌.
+i = type('egoing')
+print(i)
+print(type(['egoing','leezche','graphittie']))
+names = ['egoing','leezche','graphittie']
+print(names[0])
---
+++ loop.py
@@ -0,0 +1,6 @@
+members = ['Hyung', 'Joo', 'Kim']
+
+i = 0
+while i < 3:
+    print(members[i])
+    i = i + 1
